918012c53fa0c85ba7f6b5758fffd61137fa45f7
niyonkurueric <76616593+niyonkurueric@users.noreply.github.com>
2022-07-04 13:45:30 -0400

    Signup (#2)
    * collection for signup
    
    * removing node modules
    
    * collection of controller
    
    * finalization of signup
    
    * finalization of signup
    
    * signup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+import './db/mongoose'
 import express from 'express';
 import routes from './routes';
 import 'dotenv/config'
 const port=process.env.PORT|| 4000
 const app = express();
 app.use(express.json());
-app.use("/api/v1",routes)
+app.use("/api/v1",routes);
 app.listen(port,()=>console.log(`The server is running on port ${port}`))
---
+++ src/helpers/passwordSecurity.js
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt'
+
+const salt = bcrypt.genSaltSync(10, 'b')
+
+export const hashPassword = (plainPassword) => {
+    const hash = bcrypt.hashSync(plainPassword, salt)
+    return hash
+}
